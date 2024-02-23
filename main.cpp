8183a21ce4e31703c7e0891be0e00e66515dc07b
nikita <82268713+nikiittaa@users.noreply.github.com>
2024-02-23 12:08:50 +0000

    поміняв місцями 8 і 9 рядки з 11 і 12

--- main.cpp
+++ main.cpp
@@ -5,11 +5,11 @@ int main() {
     int a, b, op;
     double result;
 
-    cout << "Введите первое число\n";
-    cin >> a;
-
     cout << "Введите второе число\n";
     cin >> b;
+  
+    cout << "Введите первое число\n";
+    cin >> a;
     
     cout << "Введите оператор\n1\t+\n2\t-\n3\t*\n4\t/\n\n";
     cin >> op;
