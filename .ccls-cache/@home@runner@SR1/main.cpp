d59af4a9a07c78b1e987c23d4e7f2a7b0158dd01
nikmark1212 <32316335-nikmark1212@users.noreply.replit.com>
2024-02-23 11:54:09 +0000

    поміняв місцями 11 і 12 рядки з 14 і 15

--- .ccls-cache/@home@runner@SR1/main.cpp
+++ .ccls-cache/@home@runner@SR1/main.cpp
@@ -1,27 +1,35 @@
 #include <iostream>
 using namespace std;
+
 int main() {
- int a, b, op;
- double result;
- cout << "Введіть перше число\n";
- cin >> a;
- cout << "Введіть оператор\n1\t+\n2\t-\n3\t*\n4\t/\n\n";
- cin >> op;
- cout << "Введіть друге число\n";
- cin >> b;
- switch(op) {
- case 1:
- result = a + b;
- break;
- case 2:
- result = a - b;
- break;
- case 3:
- result = a * b;
- break;
- case 4:
- result = (double) a / b;
- break;
- }
- cout << "Результат = " << result;
+    int a, b, op;
+    double result;
+
+    cout << "Введите первое число\n";
+    cin >> a;
+
+    cout << "Введите второе число\n";
+    cin >> b;
+
+    cout << "Введите оператор\n1\t+\n2\t-\n3\t*\n4\t/\n\n";
+    cin >> op;
+
+    switch (op) {
+        case 1:
+            result = a + b;
+            break;
+        case 2:
+            result = a - b;
+            break;
+        case 3:
+            result = a * b;
+            break;
+        case 4:
+            result = a / b;
+            break;
+    }
+
+    cout << "Результат = " << result;
+
+    return 0;
 }
